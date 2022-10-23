b74c378b4e8364b7b8bc2890ea23feafe044310f
HaifengXiao-29 <70769700+HaifengXiao-29@users.noreply.github.com>
2022-10-22 22:42:51 -0400

    fix database

--- app/utility/JDBC.java
+++ app/utility/JDBC.java
@@ -7,7 +7,8 @@ public class JDBC {
 //    static final String Url = "jdbc:mysql://mysql:3306/db";
     static final String Url = "jdbc:mysql://localhost:3306/cse312?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true";
     static final String User = "root" ;
-    static final String PassWord = "jia893607219";
+    //static final String PassWord = "jia893607219";
+    static final String PassWord = "JayX2029";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {;
         Class.forName(JdbcDriver);
@@ -26,8 +27,11 @@ public class JDBC {
         String sql = "create table if not exists userTable "+
                     "(id INT AUTO_INCREMENT, " +
                     "email VARCHAR(516), " +
+                    "firstname VARCHAR(516)," +
+                    "lastname VARCHAR(516)," +
                     "password VARCHAR(516), "+
-                    "PRIMARY KEY (id));";
+                    "courseId INT," +
+                    "KEY (id));";
         statement.executeUpdate(sql);
         statement.close();
         return connection;
