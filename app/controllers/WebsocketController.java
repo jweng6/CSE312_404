293fecf590b36402cc166b86ad7490d757aacf31
chuanlon <chuanlon@buffalo.edu>
2022-12-07 04:16:22 -0500

    websocket

--- app/controllers/WebsocketController.java
+++ app/controllers/WebsocketController.java
@@ -2,12 +2,15 @@ package controllers;
 
 import akka.actor.ActorSystem;
 import akka.stream.Materializer;
+import play.libs.F;
 import play.libs.streams.ActorFlow;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.WebSocket;
 import utility.MyWebSocketActor;
 
 import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
 
 public class WebsocketController extends Controller {
     private final ActorSystem actorSystem;
@@ -18,11 +21,25 @@ public class WebsocketController extends Controller {
         this.actorSystem = actorSystem;
         this.materializer = materializer;
     }
-
     public WebSocket socket(String code) {
+        System.out.println(code);
         return WebSocket.Json.accept(
                 request -> ActorFlow.actorRef(MyWebSocketActor::props, actorSystem, materializer));
     }
+//    public WebSocket socket(String code) {
+//        System.out.println(code);
+//        return WebSocket.Json.accept(
+//                request -> ActorFlow.actorRef(MyWebSocketActor::props, actorSystem, materializer));
+//    }
+//    public WebSocket socket() {
+//        return WebSocket.Json.accept(
+//                request -> CompletableFuture.completedFuture(
+//
+//                        F.Either.Right(f.apply(request)))
+//        );
+////                request -> ActorFlow.actorRef(MyWebSocketActor::props, actorSystem, materializer));
+//    }
+
 
 
 
--- app/utility/Constant.java
+++ app/utility/Constant.java
@@ -1,13 +1,15 @@
 package utility;
 
 import akka.actor.ActorRef;
-
 import java.util.ArrayList;
 import java.util.List;
+import domain.socketActor;
 
 public class Constant {
     public static String SALT="404";
     public static List<ActorRef> list = new ArrayList<>();
+    int code = 123;
+    public static List<socketActor> ClientList = new ArrayList<>();
 
     public static String injection(String word){
         return word.replace("&", "&amp;")
